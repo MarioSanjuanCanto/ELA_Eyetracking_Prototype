494259486829e83ba66a7421eaeb1a2a7a147777
Mario Sanjuan Canto <msancan3@upv.edu.es>
2026-05-04 14:10:46 +0200

    fix: path conflict

--- demoCloneVoice/vite.config.ts
+++ demoCloneVoice/vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from "vite";
-import { tanstackRouter } from "@tanstack/router-plugin/vite";
-import { tanstackStart } from "@tanstack/react-start/vite";
+import react from "@vitejs/plugin-react";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
-    tanstackRouter(),
     tanstackStart(),
+    react(),
     tsconfigPaths(),
     tailwindcss(),
   ],
